20946383c5967c345f522ee4f0e5cf78ec11913b
Iago Bontempo <iagobontempo@hotmail.com>
2020-02-01 11:49:49 -0300

    Migrations created

--- knexfile.js
+++ knexfile.js
@@ -2,29 +2,21 @@
 
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   }
-  // },
-
-  production: {
-    client: 'mysql',
-    version: '5.7',
-    connection: {
-      host: 'localhost',
-      user: 'root',
-      password: '123456',
-      database: 'node_api'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'mysql',
+  version: '5.7',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'node_api'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
+
 
 };
